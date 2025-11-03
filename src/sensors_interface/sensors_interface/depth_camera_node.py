b76c1abe4e0211f7fec02d244106ac21ecb8a235
aabougendia <aabugendia@gmail.com>
2025-11-03 03:53:28 +0000

    setting up sensors_interface/depth_camera

---
+++ src/sensors_interface/sensors_interface/depth_camera_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from sensors_interface.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/sensors_interface/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+package_name = 'sensors_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        
+        # depth camera
+        ('share/' + package_name + '/launch', ['launch/depth_camera_sim.launch.py']),
+        ('share/' + package_name + '/urdf', ['urdf/depth_camera.xacro']),
+        # ('share/' + package_name + '/rviz', ['rviz/depth_camera.rviz']),
+        ('share/' + package_name +'/worlds', ['worlds/empty.world']),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aabougen',
+    maintainer_email='aabugendia@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'depth_camera_node = sensors_interface.depth_camera_node:main'
+        ],
+    },
+)
